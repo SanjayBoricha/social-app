f5b4f8597e515a19ada6284122d5de2ea6292c99
SanjayBoricha <sanjayboricha1001@gmail.com>
2020-11-08 16:23:33 +0530

    installed packages and generated components, pages and service files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './components/post/post.component';
+import { UserComponent } from './components/user/user.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AddPostComponent } from './pages/add-post/add-post.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SigninComponent } from './pages/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    UserComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AddPostComponent,
+    PageNotFoundComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
